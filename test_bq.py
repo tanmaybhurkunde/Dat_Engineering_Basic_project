cdadaa3460a23fad2a5c1ab02aa528a55517d5b8
tanma <tanmaybhurkhunde2018@gmail.com>
2026-06-07 21:25:28 +0530

    Remove test file

--- test_bq.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import bigquery
-
-client = bigquery.Client(project="etl-weather-pipeline-498619")
-print(f"Connected to project: {client.project}")
-
-# List datasets to confirm connection
-datasets = list(client.list_datasets())
-print(f"Datasets found: {[d.dataset_id for d in datasets]}")
